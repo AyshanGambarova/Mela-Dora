54a172db8445f46d34f490654133d2b12e894ac6
User <User@WINDOWS-1TDC0VN>
2022-09-03 22:47:02 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function (e) {
+    navbar()
+    $(window).on("scroll", function () {
+        navbar();
+    })
+    function navbar() {
+        if ($(window).scrollTop() > 30) {
+            $("#main-header").addClass("scrolled");
+            $(".navbar-top").css('display', 'none');
+            $(".navbar-brand img").attr("src", "images/logo-scrolled.svg");
+            $(".scroll-top").css({ opacity: 1 });
+            console.log("salam");
+            
+        }
+        else {
+            $("#main-header").removeClass("scrolled");
+            $(".navbar-top").css('display', 'block');
+            $(".navbar-brand img").attr("src", "images/logo-primary.svg");
+            $(".scroll-top").css({ opacity: 0 });
+
+
+        }
+    }
+
+
+
+
+
+ 
+});

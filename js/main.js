7ee35d472d79b482fead508834be63e46bb88df7
User <User@WINDOWS-1TDC0VN>
2022-09-06 15:57:26 +0400

    last commit of mela-dora

--- js/main.js
+++ js/main.js
@@ -2,24 +2,45 @@ $(document).ready(function (e) {
     navbar()
     $(window).on("scroll", function () {
         navbar();
+    });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 3,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1024: {
+                items: 3
+            }
+        }
     })
     function navbar() {
         if ($(window).scrollTop() > 30) {
-            $("#main-header").addClass("scrolled");
+            $("#main-header,#about-header").addClass("scrolled");
+            $("#about-header .navbar-nav a.nav-link").addClass("text-white");
             $(".navbar-top").css('display', 'none');
             $(".navbar-brand img").attr("src", "images/logo-scrolled.svg");
             $(".scroll-top").css({ opacity: 1 });
+         
             
         }
         else {
-            $("#main-header").removeClass("scrolled");
-            $(".navbar-top").css('display', 'block');
-            $(".navbar-brand img").attr("src", "images/logo-primary.svg");
+            $("#main-header,#about-header").removeClass("scrolled");
+            $("#about-header .navbar-nav a.nav-link").removeClass("text-white");
+            $(".navbar-top").css('display', 'flex');
+            $("#main-header .navbar-brand img").attr("src", "images/logo-primary.svg");
+            $("#about-header .navbar-brand img").attr("src", "images/logo-tertiary-dark.svg");
             $(".scroll-top").css({ opacity: 0 });
 
 
         }
     }
+
 
 
 
